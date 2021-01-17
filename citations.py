9562ea64b9a6daf5a76ec4ba0c5694b3b0a3ac08
chrisnicollo <72950547+chrisnicollo@users.noreply.github.com>
2021-01-17 12:40:38 -0500

    final commit

---
+++ citations.py
@@ -0,0 +1,7 @@
+# To take time input: https://www.codevscolor.com/date-valid-check-python
+# Appending to lists: https://www.geeksforgeeks.org/python-append-string-to-list/
+# Finding the length of a list: https://www.edureka.co/blog/python-list-length/
+# Creating desktop notifications: https://github.com/jithurjacob/Windows-10-Toast-Notifications/blob/master/README.md
+# Inserting an item into a list: https://developers.google.com/edu/python/lists, https://www.programiz.com/python-programming/methods/list/insert
+# Deleting an item from a list: https://note.nkmk.me/en/python-list-clear-pop-remove-del/
+# Editing an item in a list: https://datatofish.com/modify-list-python/
